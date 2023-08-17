c8dc7610d5f817a3bcdec5df499eae5f9a8ffcdd
faisal-ffs <faisalsabir.ffs@gmail.com>
2023-08-17 17:52:01 +0500

    deploy

--- src/Components/Bvalue/index.js
+++ src/Components/Bvalue/index.js
@@ -11,35 +11,35 @@ function Index() {
     <section className='mx-auto mt-8 max-w-6xl bg-slate-100 pt-8'>
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4 md:px-12 lg:px-24 '>
         <div className='w-full'>
-          <div className='relative ml-4'>
+          <div className='relative flex justify-center'>
             <img src={whg} alt='Image 1' />
           </div>
           <div className='w-full mt-8'>
-          <div className='mt-4 font-sans ml-4 text-sm md:text-base lg:text-lg'>Pitch termsheet backing validation focus release value proposition..</div>
-          <div className='text-start flex  flex-row'>
-            <img src={random} alt='random' className='w-8 h-8 ml-4 rounded-full mt-4' />
-            <p className='ml-4 mt-6 font-sans'>Chandler Bing</p>
+            <div className='mt-4 font-sans ml-8 text-sm md:text-base lg:text-lg'>Pitch termsheet backing validation focus release value proposition..</div>
+            <div className='text-start flex  flex-row'>
+              <img src={random} alt='random' className='w-8 h-8 ml-8 rounded-full mt-4' />
+              <p className='ml-4 mt-6 font-sans'>Chandler Bing</p>
+            </div>
           </div>
-        </div>
         </div>
         <div className='w-full mt-8 sm:mt-0'>
-          <div className='relative ml-4'>
+          <div className='relative justify-center flex'>
             <img src={girl} alt='girl' />
           </div>
-          <div className='mt-4 font-sans ml-4'>Seed round direct mailing non-disclosure agreement graphical user interface rockstar.</div>
+          <div className='mt-4 font-sans ml-8'>Seed round direct mailing non-disclosure agreement graphical user interface rockstar.</div>
           <div className='text-start flex flex-row'>
-            <img src={random1} alt='random1' className='w-8 h-8 ml-4 rounded-full mt-4' />
+            <img src={random1} alt='random1' className='w-8 h-8 ml-8 rounded-full mt-4' />
             <p className='ml-4 mt-6 font-sans'>Rachel Green</p>
           </div>
         </div>
         <div className='w-full mt-8 sm:mt-0'>
-          <div className='relative ml-4'>
+          <div className='relative  flex justify-center'>
             <img src={trash} alt='Image 3' />
           </div>
-          <div className='mt-4 ml-4 font-sans'>Beta prototype sales iPad Gen-Z marketing network effect value proposition.</div>
+          <div className='mt-4 ml-8 font-sans'>Beta prototype sales iPad Gen-Z marketing network effect value proposition.</div>
           <div className='text-start flex flex-row'>
-            <img src={random2} alt='random2' className='w-8 h-8 ml-4 rounded-full mt-4' />
-            <p className='ml-4 mt-6 font-sans'>Monica Geller</p>
+            <img src={random2} alt='random2' className='w-8 h-8 ml-8  rounded-full mt-4' />
+            <p className=' ml-4  mt-6 font-sans'>Monica Geller</p>
           </div>
         </div>
       </div>
